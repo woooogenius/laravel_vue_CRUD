732e050852bf9ff0c1e512fbd2edf769e6385c91
최우진 <woojin@Amuz-Woojins-MacBook.local>
2024-03-25 17:49:45 +0900

    model

---
+++ app/Models/Posting.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Posting extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'content',
+
+    ];
+
+    protected $table = 'post';
+
+
+
+}
